4e1fb6eca0feeeef1537f2dea6890e205e8c8a32
Clare0808 <yuhsin099@gmail.com>
2026-06-24 23:41:02 +0800

    建立CartPage

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,7 +1,39 @@
+import style from "./CartPage.module.css";
+
+import dishImg from "../assets/img/homepage1.png";
+
+import { FaTrash } from "react-icons/fa";
+import { MdOutlineIndeterminateCheckBox } from "react-icons/md";
+
 function CartPage () {
     return (
         <>
-            <div>Cart</div>
+            <div className={style.title}>購物車</div>
+            <div className={style.contentFrame}>
+                <div className={style.dishBox}>
+                    <img src={dishImg} />
+                    <div className={style.dishInfoFrame}>
+                        <div className={style.dishInfo}>
+                            <div className={style.dishName}>Test</div>
+                            <div className={style.numBox}>
+                                <div className={style.numBtn}>-</div>
+                                <div className={style.dishAmount}>1</div>
+                                <div className={style.numBtn}>+</div>
+                            </div>
+                        </div>
+                        <div className={style.price}>$1000</div>
+                    </div>
+                    <div className={style.funcFrame}>
+                        <input className={style.checkbox} type="checkbox" />
+                        <FaTrash className={style.cancelBtn} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.endBan}>
+                <div className={style.totalPrice}>$1000</div>
+                <div className={style.btn}>結帳</div>
+            </div>
         </>
     )
 }
